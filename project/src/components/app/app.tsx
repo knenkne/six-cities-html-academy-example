0427d6300f7f42fbe9e84e83d9f1fa3ea3260745
knenkne <jztenk@gmail.com>
2022-07-14 17:34:48 +0300

    [Homework-3] add routes for each page

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,11 +1,25 @@
-import { Main } from '../../pages/main/main';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+import Main from '../../pages/main/main';
+import Login from '../../pages/login/login';
+import Favorites from '../../pages/favorites/favorites';
+import Property from '../../pages/property/property';
+import NotFound from '../../pages/not-found/not-found';
 
 type AppProps = {
   offersCount: number
 }
 
-function App({ offersCount }: AppProps): JSX.Element {
-  return <Main offersCount={offersCount} />;
-}
+const App = ({ offersCount }: AppProps): JSX.Element => (
+  <BrowserRouter>
+    <Routes>
+      <Route index element={<Main offersCount={offersCount} />}/>
+      <Route path="/login" element={<Login />} />
+      <Route path="/favorites" element={<Favorites />} />
+      <Route path="/offer/:id" element={<Property />} />
+      <Route path="*" element={<NotFound />} />
+    </Routes>
+  </BrowserRouter>
+);
 
 export default App;
