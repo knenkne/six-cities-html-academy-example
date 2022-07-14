c941a81e50d9599b0f0ad33339e2435db938e283
knenkne <jztenk@gmail.com>
2022-07-14 17:52:23 +0300

    [Homework-3] add private route component

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -5,6 +5,8 @@ import Login from '../../pages/login/login';
 import Favorites from '../../pages/favorites/favorites';
 import Property from '../../pages/property/property';
 import NotFound from '../../pages/not-found/not-found';
+import PrivateRoute from '../private-route/private-route';
+import { AppRoute, AuthorizationStatus } from '../../const';
 
 type AppProps = {
   offersCount: number
@@ -14,9 +16,17 @@ const App = ({ offersCount }: AppProps): JSX.Element => (
   <BrowserRouter>
     <Routes>
       <Route index element={<Main offersCount={offersCount} />}/>
-      <Route path="/login" element={<Login />} />
-      <Route path="/favorites" element={<Favorites />} />
-      <Route path="/offer/:id" element={<Property />} />
+      <Route path={AppRoute.Login} element={<Login />} />
+      <Route path={`${AppRoute.Property}/:id`} element={<Property />} />
+      <Route path={AppRoute.Favorites}
+        element={
+          <PrivateRoute
+            authorizationStatus={AuthorizationStatus.NoAuth}
+          >
+            <Favorites />
+          </PrivateRoute>
+        }
+      />
       <Route path="*" element={<NotFound />} />
     </Routes>
   </BrowserRouter>
