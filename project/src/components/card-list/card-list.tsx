45c9e54b7d48001b1fc9af0358dba4c4cbf13b00
knenkne <jztenk@gmail.com>
2022-08-02 01:14:41 +0300

    [Homework-6] active pin change

--- project/src/components/card-list/card-list.tsx
+++ project/src/components/card-list/card-list.tsx
@@ -14,7 +14,6 @@ const CardList = (): JSX.Element => {
   const activeSorting = useAppSelector((state) => state.sorting);
   const activeCity = useAppSelector((state) => state.city);
   const offers = useAppSelector((state) => state.offers.filter((offer) => offer.city.name === state.city.name).sort(Comprator[state.sorting]));
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const [activeOffer, setActiveOffer] = useState<number | null>(null);
 
   const handleCardMouseMove = (id: number) => {
@@ -25,7 +24,7 @@ const CardList = (): JSX.Element => {
     setActiveOffer(null);
   };
 
-  const handleSortingChange = (name: Sort) => {
+  const onSortingChange = (name: Sort) => {
     dispatch(setSorting(name));
   };
 
@@ -34,7 +33,7 @@ const CardList = (): JSX.Element => {
       <section className="cities__places places">
         <h2 className="visually-hidden">Places</h2>
         <b className="places__found">{offers.length} places to stay in {activeCity.name}</b>
-        <Sorting onChange={handleSortingChange} activeSorting={activeSorting} />
+        <Sorting onChange={onSortingChange} activeSorting={activeSorting} />
         <div className="cities__places-list places__list tabs__content">
           {offers.map((offer) => (
             <Card
@@ -47,7 +46,7 @@ const CardList = (): JSX.Element => {
         </div>
       </section>
       <div className="cities__right-section">
-        <Map locations={offers.map((offer) => offer.location)} city={activeCity} />
+        <Map locations={offers.map(({ id, location }) => ({ id, ...location }))} city={activeCity} activeOffer={activeOffer} />
       </div>
     </>
   );
