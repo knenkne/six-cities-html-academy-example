2c09ef0a2d11f56a2eab46ef29047eaf91722d80
knenkne <jztenk@gmail.com>
2022-08-03 03:47:22 +0300

    [Homework-7] removing mocks

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -6,16 +6,14 @@ import Favorites from '../../pages/favorites/favorites';
 import Property from '../../pages/property/property';
 import NotFound from '../../pages/not-found/not-found';
 import PrivateRoute from '../private-route/private-route';
-import { AppRoute, AuthorizationStatus } from '../../const';
-import city from '../../mocks/city';
-import reviews from '../../mocks/reviews';
+import { AppRoute, AuthorizationStatus, CityLocation } from '../../const';
 
 const App = (): JSX.Element => (
   <BrowserRouter>
     <Routes>
       <Route index element={<Main />} />
       <Route path={AppRoute.Login} element={<Login />} />
-      <Route path={`${AppRoute.Property}/:id`} element={<Property city={city} nearbyOffers={[]} reviews={reviews} />} />
+      <Route path={`${AppRoute.Property}/:id`} element={<Property city={{ name: 'Amsterdam', location: CityLocation.Amsterdam }} nearbyOffers={[]} reviews={[]} />} />
       <Route
         path={AppRoute.Favorites}
         element={
