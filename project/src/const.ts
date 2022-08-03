571aac305575eb2b062fe61d3b0f744c416f493f
knenkne <jztenk@gmail.com>
2022-08-04 01:13:11 +0300

    [Homework-7] change cities zoom

--- project/src/const.ts
+++ project/src/const.ts
@@ -46,31 +46,31 @@ export const CityLocation: { [key in CityName]: Location } = {
   'Paris': {
     latitude: 48.85661,
     longitude: 2.351499,
-    zoom: 10
+    zoom: 13
   },
   'Cologne': {
     latitude: 50.938361,
     longitude: 6.959974,
-    zoom: 10
+    zoom: 13
   },
   'Brussels': {
     latitude: 50.846557,
     longitude: 4.351697,
-    zoom: 10
+    zoom: 13
   },
   'Amsterdam': {
     latitude: 52.37454,
     longitude: 4.897976,
-    zoom: 10
+    zoom: 13
   },
   'Hamburg': {
     latitude: 53.550341,
     longitude: 10.000654,
-    zoom: 10
+    zoom: 13
   },
   'Dusseldorf': {
     latitude: 51.225402,
     longitude: 6.776314,
-    zoom: 10
+    zoom: 13
   },
 };
