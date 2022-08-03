382ba9ff472da449585b5950100c3e82cb788007
knenkne <jztenk@gmail.com>
2022-08-03 03:44:57 +0300

    [Homework-7] fix timeout value

--- project/src/api.ts
+++ project/src/api.ts
@@ -1,7 +1,7 @@
 import axios, { AxiosInstance } from 'axios';
 
 const BACKEND_URL = 'https://10.react.pages.academy/six-cities';
-const REQUEST_TIMEOUT = 500;
+const REQUEST_TIMEOUT = 5000;
 
 export const createAPI = (): AxiosInstance => {
   const api = axios.create({
