96a5d3a23a61417a52f581a15ceeb8185de02b62
knenkne <jztenk@gmail.com>
2022-08-17 18:37:23 +0300

    [Homework-9] Header as Layout

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -9,29 +9,32 @@ import NotFound from '../../pages/not-found/not-found';
 import PrivateRoute from '../private-route/private-route';
 import { AppRoute, AuthorizationStatus } from '../../const';
 import history from '../../history';
+import Header from '../header/header';
 
 const App = (): JSX.Element => (
   <HistoryRouter history={history}>
     <Routes>
-      <Route index element={<Main />} />
-      <Route path={`${AppRoute.Property}/:id`} element={<Property />} />
-      <Route
-        path={AppRoute.Login}
-        element={
-          <PrivateRoute restrictedFor={AuthorizationStatus.Auth} redirectTo={AppRoute.Root}>
-            <Login />
-          </PrivateRoute>
-        }
-      />
-      <Route
-        path={AppRoute.Favorites}
-        element={
-          <PrivateRoute restrictedFor={AuthorizationStatus.NoAuth} redirectTo={AppRoute.Login}>
-            <Favorites />
-          </PrivateRoute>
-        }
-      />
-      <Route path="*" element={<NotFound />} />
+      <Route element={<Header />}>
+        <Route index element={<Main />} />
+        <Route path={`${AppRoute.Property}/:id`} element={<Property />} />
+        <Route
+          path={AppRoute.Favorites}
+          element={
+            <PrivateRoute restrictedFor={AuthorizationStatus.NoAuth} redirectTo={AppRoute.Login}>
+              <Favorites />
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path={AppRoute.Login}
+          element={
+            <PrivateRoute restrictedFor={AuthorizationStatus.Auth} redirectTo={AppRoute.Root}>
+              <Login />
+            </PrivateRoute>
+          }
+        />
+        <Route path="*" element={<NotFound />} />
+      </Route>
     </Routes>
   </HistoryRouter>
 );
